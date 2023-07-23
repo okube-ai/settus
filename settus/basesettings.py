11950090ff2083fe30a031927d72138210d4415d
osoucy <osoucy.transactions@gmail.com>
2023-07-23 02:32:25 -0400

    Update

--- settus/basesettings.py
+++ settus/basesettings.py
@@ -1,3 +1,8 @@
+import pydantic
+print("pydantic version", pydantic.__version__)
+import pydantic_settings
+print("pydantic settings version", pydantic_settings.__version__)
+
 from pydantic_settings import BaseSettings as _BaseSettings
 
 
