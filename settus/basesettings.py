3984061d9aad11ff2dcde900b384489125aeeac4
osoucy <osoucy.transactions@gmail.com>
2023-07-23 03:44:58 -0400

    Update

--- settus/basesettings.py
+++ settus/basesettings.py
@@ -1,8 +1,3 @@
-import pydantic
-print("pydantic version in venv", pydantic.__version__)
-import pydantic_settings
-print("pydantic settings version in venv", pydantic_settings.__version__)
-
 from pydantic_settings import BaseSettings as _BaseSettings
 
 
