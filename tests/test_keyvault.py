3be53c1eb8946621224dd29f6398120749c59c84
osoucy <osoucy.transactions@gmail.com>
2023-09-20 02:07:47 -0400

    Update

--- tests/test_keyvault.py
+++ tests/test_keyvault.py
@@ -12,16 +12,16 @@ os.environ["TOP"] = "v2"
 KEYVAULT_URL = "https://kv-laktory-dev.vault.azure.net/"
 
 
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(keyvault_url=KEYVAULT_URL)
-    env_1: str = Field(default="undefined")
-    top: str = Field(default="undefined")
-    kv_1: str = Field(default="undefined", alias="my-secret")
-    kv_2: str = Field(default="undefined", alias=AliasChoices("ENV_1", "my-secret"))
-    kv_3: str = Field(default="undefined", alias="my-secret", keyvault_url=KEYVAULT_URL)
-
-
 def test_keyvault():
+
+    class Settings(BaseSettings):
+        model_config = SettingsConfigDict(keyvault_url=KEYVAULT_URL)
+        env_1: str = Field(default="undefined")
+        top: str = Field(default="undefined")
+        kv_1: str = Field(default="undefined", alias="my-secret")
+        kv_2: str = Field(default="undefined", alias=AliasChoices("ENV_1", "my-secret"))
+        kv_3: str = Field(default="undefined", alias="my-secret", keyvault_url=KEYVAULT_URL)
+
     settings = Settings()
     assert settings.env_1 == "v1"
     assert settings.kv_1 == "secretsauce"
@@ -29,5 +29,17 @@ def test_keyvault():
     assert settings.kv_2 == "v1"
 
 
+def test_name_conflicts():
+
+    class Settings(BaseSettings):
+        s1: str = Field(default="s1")
+        s2: str = Field(default="s2", alias="env_1")
+
+    settings = Settings(s2="value_from_init")
+    assert settings.s2 == "value_from_init"
+
+
 if __name__ == "__main__":
-    test_keyvault()
+    # test_keyvault()
+    test_name_conflicts()
+
